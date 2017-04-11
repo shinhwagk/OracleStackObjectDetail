23c19abb18cbdf5abc833e2bb3ff54e3414901c1
gk <shanghai_sl@hotmail.com>
2017-04-11 22:42:23 +0800

    first

--- frontend/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'orcl-obj',
-  template: `<router-outlet></router-outlet>`,
-})
-export class AppComponent { name = 'Angular'; }
